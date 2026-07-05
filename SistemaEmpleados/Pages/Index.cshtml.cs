351da60a75678af3f29c293026a13e969a0ce3f1
ranmanew <ranmanew@DESKTOP-T9HTFSS>
2026-07-05 18:07:59 -0400

    Vista Razor

---
+++ SistemaEmpleados/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SistemaEmpleados.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
